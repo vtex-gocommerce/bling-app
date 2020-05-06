865fd77357660819ac9c42545f6e4e108ce1cc5b
Augusto Lazaro <aemlazaro@gmail.com>
2020-05-06 14:20:44 -0300

    Refactor bling form

--- react/BlingForm.tsx
+++ react/BlingForm.tsx
@@ -1,78 +1,53 @@
 import * as React from 'react'
-import { WithNavigate, Form } from 'gocommerce.gc-utils'
-import { FormattedMessage } from 'react-intl'
-import { Link } from 'vtex.render-runtime'
+import { Form } from 'gocommerce.gc-utils'
+import { useIntl } from 'react-intl'
+
+import { useRuntime } from 'vtex.render-runtime'
 import { TemplatePage } from 'gocommerce.gc-utils'
 import { Alert, Notify, Container, Modal } from 'gocommerce.styleguide'
-import { Button } from 'vtex.styleguide'
+import { Button, Layout, PageHeader } from 'vtex.styleguide'
+
 import PlaceHolderContainerCard from './components/placeHolderContainerCard'
 
 interface BlingFormProps {
   account: any
-  intl: any
-  navigate?: Function
   blingConfig: any
   generateAppCredentials: Function
   errorGenerateAppCredentials: any
   isLoadingGenerateAppCredentials: boolean
 }
 
-interface BlingFormState {
-  appKey: string
-  appToken: string
-  generated: boolean
-  openModal: boolean
-}
+const BlingForm = ({
+  account,
+  blingConfig,
+  isLoadingGenerateAppCredentials,
+  generateAppCredentials,
+}: BlingFormProps) => {
+  const [openModal, setOpenModal] = React.useState(false)
+  const [generated, setGenerated] = React.useState(false)
+  const [appKey, setAppKey] = React.useState(blingConfig.appKey)
+  const [appToken, setAppToken] = React.useState(blingConfig.appToken)
 
-class BlingForm extends React.PureComponent<BlingFormProps, BlingFormState> {
-  constructor(props) {
-    super(props)
+  const { formatMessage } = useIntl()
+  const { navigate } = useRuntime()
 
-    this.state = {
-      appKey: props.blingConfig.appKey,
-      appToken: props.blingConfig.appKey,
-      generated: false,
-      openModal: false
-    }
-  }
-
-  formatMessage = (id: string) => this.props.intl.formatMessage({ id }, {})
-
-  handleClickButton = () => {
-    this.setState({
-      openModal: true
-    })
-  }
-
-  modalOnClose = () => {
-    this.setState({
-      openModal: false
-    })
-  }
-
-  handleSubmitForm = values => {
-    this.setState({
-      openModal: false
-    })
-
-    this.props
-      .generateAppCredentials({ variables: { data: JSON.stringify(values) } })
-      .then(response => {
+  const handleSubmitForm = (values: any) => {
+    setOpenModal(true)
+    generateAppCredentials({ variables: { data: JSON.stringify(values) } })
+      .then(({ data: { generateCredentials }}) => {
         if (
-          response.data.generateCredentials.status === 'success' &&
-          response.data.generateCredentials.userErrors.length === 0
+          generateCredentials.status === 'success' &&
+          generateCredentials.userErrors.length === 0
         ) {
-          Notify.show(this.props.intl.formatMessage({ id: 'admin/appBling.textSugestion' }), {
+          Notify.show(formatMessage({ id: 'admin/appBling.textSugestion' }), {
             position: 'top-right',
             type: 'success'
           })
 
-          this.setState({
-            openModal: false,
-            appKey: response.data.generateCredentials.key,
-            appToken: response.data.generateCredentials.secret,
-            generated: true
-          })
+          setOpenModal(false)
+          setAppKey(generateCredentials.key)
+          setAppToken(generateCredentials.secret)
+          setGenerated(true)
         }
       })
       .catch(error => {
@@ -80,143 +55,133 @@ class BlingForm extends React.PureComponent<BlingFormProps, BlingFormState> {
       })
   }
 
-  render() {
-    const { blingConfig, account, isLoadingGenerateAppCredentials } = this.props
-    const isLoadingData = isLoadingGenerateAppCredentials ? true : false
+  const handleBack = () => {
+    return navigate({
+      page: 'admin.app.apps.setup',
+      params: { appId: 'gocommerce.bling-app@2.x' },
+    })
+  }
 
-    const breadcrumbConfig = [
-      { title: 'Apps', page: 'admin.apps.home' },
-      { title: <FormattedMessage id="admin/appBling.partner" /> }
-    ]
+  return (
+    <TemplatePage>
+      <Layout
+        pageHeader={
+          <PageHeader
+            title={formatMessage({ id: 'admin/appBling.partner' })}
+            linkLabel={formatMessage({ id: 'admin/appBling.settings' })}
+            onLinkClick={handleBack}
+          />
+        }
+      >
+        {isLoadingGenerateAppCredentials ? (
+          <PlaceHolderContainerCard isPlaceholderActive={true}>{() => <div />}</PlaceHolderContainerCard>
+        ) : (
+          <div>
+            {generated && (
+              <div className="g-mb2">
+                <Alert title={formatMessage({ id: 'admin/appBling.attentionTitle' })} type="warning">
+                  {formatMessage({ id: 'admin/appBling.attentionInfo' })}
+                </Alert>
+              </div>
+            )}
 
-    return (
-      <TemplatePage>
-        <TemplatePage.Header
-          breadcrumbConfig={breadcrumbConfig}
-          buttons={
-            <div className="dn db-ns">
-              <Link className="link" page="admin.apps.home">
-                <Button size="large" variation="secondary">
-                  <FormattedMessage id="admin/appBling.buttonCancel" />
-                </Button>
-              </Link>
-            </div>
-          }
-        />
+            <Form id="FormId" onSubmit={handleSubmitForm}>
+              {stateData => (
+                <>
+                  <Container isPlaceholderActive={isLoadingGenerateAppCredentials} id="default">
+                    <h2 className="g-ma0 g-f4 fw6">
+                      {formatMessage({ id: 'admin/appBling.formTitle' })}
+                    </h2>
 
-        <TemplatePage.Content type="small">
-          {isLoadingData ? (
-            <PlaceHolderContainerCard isPlaceholderActive={true}>{() => <div />}</PlaceHolderContainerCard>
-          ) : (
-            <div>
-              {this.state.generated && (
-                <div className="g-mb2">
-                  <Alert title={this.formatMessage('admin/appBling.attentionTitle')} type="warning">
-                    <FormattedMessage id="admin/appBling.attentionInfo" />
-                  </Alert>
-                </div>
+                    <div className="g-mt6">
+                      <Form.Input
+                        label={formatMessage({ id: 'admin/appBling.account'})}
+                        className="w-100 tracked-mega g-f1"
+                        name="account"
+                        value={account.id}
+                        disabled={true}
+                      />
+                    </div>
+
+                    <div className="g-mt6">
+                      <Form.Input
+                        label={formatMessage({ id: 'admin/appBling.appKey'})}
+                        className="w-100 tracked-mega g-f1"
+                        name="appKey"
+                        value={appKey}
+                        disabled={true}
+                      />
+                    </div>
+
+                    <div className="g-mt6">
+                      <Form.Input
+                        label={formatMessage({ id: 'admin/appBling.appToken'})}
+                        className="w-100 tracked-mega g-f1"
+                        name="appToken"
+                        value={appToken}
+                        disabled={true}
+                      />
+                    </div>
+                  </Container>
+                </>
               )}
+            </Form>
 
-              <Form id="FormId" onSubmit={this.handleSubmitForm}>
-                {stateData => (
-                  <>
-                    <Container isPlaceholderActive={this.props.isLoadingGenerateAppCredentials} id="default">
-                      <h2 className="g-ma0 g-f4 fw6">
-                        <FormattedMessage id="admin/appBling.formTitle" />
-                      </h2>
+            <div className="flex justify-between">
+              <Button size="large" variation="secondary" onClick={handleBack}>
+                {formatMessage({ id: 'admin/appBling.buttonCancel' })}
+              </Button>
 
-                      <div className="g-mt6">
-                        <Form.Input
-                          label={this.formatMessage('admin/appBling.account')}
-                          className="w-100 tracked-mega g-f1"
-                          name="account"
-                          value={account.id}
-                          disabled={true}
-                        />
-                      </div>
+              <div className="fixed static-ns w-100 w-auto-ns bottom-0 left-0 z-999">
+                <Button
+                  size="large"
+                  id="openModalGenerate"
+                  onClick={() => setOpenModal(true)}
+                  isLoading={isLoadingGenerateAppCredentials}
+                >
+                  {formatMessage({ id: 'admin/appBling.buttonGenerate' })}
+                </Button>
+              </div>
+            </div>
 
-                      <div className="g-mt6">
-                        <Form.Input
-                          label={this.formatMessage('admin/appBling.appKey')}
-                          className="w-100 tracked-mega g-f1"
-                          name="appKey"
-                          value={this.state.appKey}
-                          disabled={true}
-                        />
-                      </div>
+            <Modal
+              className="w-50-l"
+              open={openModal}
+              onClose={() => setOpenModal(false)}
+              showCloseIcon={true}
+              centered={true}
+            >
+              <p className="ma0 g-f5 fw6 navy">
+                {formatMessage({ id: 'admin/appBling.confirm' })}
+              </p>
 
-                      <div className="g-mt6">
-                        <Form.Input
-                          label={this.formatMessage('admin/appBling.appToken')}
-                          className="w-100 tracked-mega g-f1"
-                          name="appToken"
-                          value={this.state.appToken}
-                          disabled={true}
-                        />
-                      </div>
-                    </Container>
-                  </>
-                )}
-              </Form>
+              <p className="ma0 g-pv4 g-f3 c-on-base-2">
+                {formatMessage({ id: 'admin/appBling.disclaimer' })}
+              </p>
 
               <div className="flex justify-between">
-                <Link className="dn dib-ns link" page="admin.apps.home">
-                  <Button size="large" variation="secondary">
-                    <FormattedMessage id="admin/appBling.buttonCancel" />
-                  </Button>
-                </Link>
-
-                <div className="fixed static-ns w-100 w-auto-ns bottom-0 left-0 z-999">
-                  <Button
-                    size="large"
-                    id="openModalGenerate"
-                    onClick={() => this.handleClickButton()}
-                    isLoading={this.props.isLoadingGenerateAppCredentials}
-                  >
-                    <FormattedMessage id="admin/appBling.buttonGenerate" />
+                <div className="dn dib-ns link">
+                  <Button size="large" onClick={() => setOpenModal(false)} variation="secondary">
+                    {formatMessage({ id: 'admin/appBling.buttonCancel' })}
                   </Button>
                 </div>
+
+                <Form.SubmitButton
+                  id="carrierSubmit"
+                  formId="FormId"
+                  disabled={isLoadingGenerateAppCredentials}
+                  className="fixed static-ns w-100 w-auto-ns bottom-0 left-0 z-999"
+                  isLoading={isLoadingGenerateAppCredentials}
+                >
+                  {formatMessage({ id: 'admin/appBling.buttonGenerate' })}
+                </Form.SubmitButton>
               </div>
-
-              <Modal
-                className="w-50-l"
-                open={this.state.openModal}
-                onClose={this.modalOnClose}
-                showCloseIcon={true}
-                centered={true}
-              >
-                <p className="ma0 g-f5 fw6 navy">
-                  <FormattedMessage id="admin/appBling.confirm" />
-                </p>
-
-                <p className="ma0 g-pv4 g-f3 c-on-base-2">
-                  <FormattedMessage id="admin/appBling.disclaimer" />
-                </p>
-
-                <div className="flex justify-between">
-                  <div className="dn dib-ns link">
-                    <Button size="large" onClick={this.modalOnClose} variation="secondary">
-                      <FormattedMessage id="admin/appBling.buttonCancel" />
-                    </Button>
-                  </div>
-
-                  <Form.SubmitButton
-                    id="carrierSubmit"
-                    formId="FormId"
-                    disabled={this.props.isLoadingGenerateAppCredentials}
-                    className="fixed static-ns w-100 w-auto-ns bottom-0 left-0 z-999"
-                    isLoading={this.props.isLoadingGenerateAppCredentials}
-                  >
-                    <FormattedMessage id="admin/appBling.buttonGenerate" />
-                  </Form.SubmitButton>
-                </div>
-              </Modal>
-            </div>
-          )}
-        </TemplatePage.Content>
-      </TemplatePage>
-    )
-  }
+            </Modal>
+          </div>
+        )}
+      </Layout>
+    </TemplatePage>
+  )
 }
 
-export default WithNavigate.HOC()(BlingForm)
+export default BlingForm
